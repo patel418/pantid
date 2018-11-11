7c0003629b2c7fde3a5e2244d29392892f70dbd3
SimronP <patel418@purdue.edu>
2018-11-10 20:41:39 -0500

    reads the correct pages, need to download images

--- scrapy/bot/bot/spiders/coverbot.py
+++ scrapy/bot/bot/spiders/coverbot.py
@@ -29,12 +29,14 @@ class Bot(scrapy.Spider):
 	src_extractor = re.compile('src="([^"]*)"')
 	tags_extractor = re.compile('alt="([^"]*)"')
 	# Define the regex we'll need to filter the returned links
-	url_matcher = re.compile('^https:\/\/www\.zara\.com\/us\/en\/') 
+	url_matcher = re.compile('^https:\/\/www\.zara\.com\/us/en') 
 	# Create a set that'll keep track of ids we've crawled
 	crawled_ids = set()
 	def start_requests(self):
 		url = "https://www.zara.com/us/en/man-jeans-slim-l675.html?v1=1079309"
+		#url = "https://www.pexels.com/"
 		yield scrapy.Request(url, self.parse)
+
 	'''def parse(self, response):
 		body = Selector(text=response.body) 
 		link_extractor = LinkExtractor(allow=PexelsScraper.url_matcher) 
@@ -44,10 +46,21 @@ class Bot(scrapy.Spider):
 			yield scrapy.Request(link, self.parse)'''
 	def is_extracted(self, url):
 	# Image urls are of type: https://www.pexels.com/photo/asphalt-blur-clouds-dawn-392010/
-		id = int(url.split('/')[-2].split('-')[-1]) 
-		if id not in Bot.crawled_ids:
-			Bot.crawled_ids.add(id) 
-			return False
+		#print ("***url about to be split: %s " % (url))
+		#print url.split('/')
+		#print ("THIS IS THE ID: %s" % (((url.split('/')[-1]).split('-')[-1]).split('.')[0]))
+		
+		id1 = ((url.split('/')[-1]).split('-'))#[-1]).split('.')[0]))
+		#print id1
+		#What i am doing wrong is that it is coming in one at a time so i must filter first and take it only if it is man and jeans
+		
+		#id1 = id1[-1].split('.')[0]
+		if id1[0] == "man" and id1[1] == "jeans":
+			#print id1
+			id1 = id1[-1].split('.')[0]
+			if id1 not in Bot.crawled_ids:
+				Bot.crawled_ids.add(id1)
+				return False
 		return True
 
 	def parse(self, response): 
@@ -64,4 +77,21 @@ class Bot(scrapy.Spider):
 		# Crawl the filtered links 
 		for link in next_links: 
 			yield scrapy.Request(link, self.parse)
+	'''
+	def start_requests(self):
+        	urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        	]
+        	for url in urls:
+            		yield scrapy.Request(url=url, callback=self.parse)
 
+    	def parse(self, response):
+       		print ("response url: %s " % (response.url))
+		page = response.url.split("/")#[-2]
+        	print page
+		#print ("page: %s" % (page))
+		filename = 'quotes-%s.html' % page
+        	with open(filename, 'wb') as f:
+            		f.write(response.body)
+        	self.log('Saved file %s' % filename)'''
