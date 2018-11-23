06a0522b522a0c2c016882c70419e96d85d6bd0d
SimronP <patel418@purdue.edu>
2018-11-23 15:28:38 -0500

    starting python

--- scrapy/bot/bot/spiders/coverbot.py
+++ scrapy/bot/bot/spiders/coverbot.py
@@ -6,27 +6,19 @@ from scrapy.linkextractor import LinkExtractor
 from scrapy.selector import Selector 
 class Bot(scrapy.Spider): 
 	name = "pant-pic-find" 
-	#src_extractor = re.compile('src="([^"]*)"')
-	tags_extractor = re.compile('alt="([^"]*)"')
 	# Define the regex we'll need to filter the returned links
-	url_matcher = re.compile('^https:\/\/www\.zara\.com\/us/en') 
 	# Create a set that'll keep track of ids we've crawled
 	crawled_ids = set()
 	def start_requests(self):
-		#url = "https://www.levi.com/US/en_US/clothing/men/jeans/c/levi_clothing_men_jeans/facets/feature-fit/straight"
-		url = "https://www.buckle.com/mens/jeans/leg-opening:bootcut?page=6"
+		url = "https://www.lordandtaylor.com/Men/Clothing/Jeans/Straight-Jeans/shop/_/N-4ztf5k?FOLDER%3C%3Efolder_id=2534374302023928&Nao=60"
 		yield scrapy.Request(url, self.parse)
 
 	
 	
 	def parse(self, response): 
 		count = 0
-		#for image in response.css("body div#root div.row_Z2vyUxT div.block_19rjgc img::attr(src)").extract():
-		for image in response.css("body div#wrapper main#content div.bottom-section a img::attr(src)").extract():
+		for image in response.css("body div#saksContainer div#saksBody div#product-container a img::attr(src)").extract():
 			count = count+1
-			#if (count % 2 != 0):
-				#add = "https:"
-				#image = add + image
 			print image
 			print count
 			yield ImgData(image_urls=[image])
